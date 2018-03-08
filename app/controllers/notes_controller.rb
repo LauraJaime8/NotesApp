1c99e1e6454f1a1bd283bf06c0e6f6559fb452ee
LauraJaime8 <laurajaimev8@gmail.com>
2018-03-08 19:21:28 +0100

    Add show and create note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,8 +1,24 @@
 class NotesController < ApplicationController
+  #GET /notes
   def index
     @notes = Note.all
   end
+
+  #GET /notes/:id
   def show
-    @notes = Note.find(params[:id])
+    @note = Note.find(params[:id])
+  end
+
+  #GET /notes/new
+  def new
+    @note = Note.new
+  end
+  #POST /notes
+
+  def create
+    @note = Note.new(title: params[:note][:title],
+                      content: params[:note][:content])
+    @note.save
+    redirect_to @note
   end
 end
