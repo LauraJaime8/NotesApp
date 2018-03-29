a5330928cc2b854252074440bd1bca0d223e02c8
LauraJaime8 <laurajaimev8@gmail.com>
2018-03-29 16:30:10 +0200

    Add new project

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'session/new'
+
+  get 'session/create'
+
+  get 'session/destroy'
+
+  get "logout" => "session#destroy", :as => "logout"
+  get "login" => "session#new", :as => "login"
+  post "login" => "session#create"
+  get "signup" => "users#new", :as => "signup"
+
+  resources :notes
+  resources :users
+  resources :session
+
+  root :to => "notes#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
