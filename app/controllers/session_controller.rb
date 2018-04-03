3f9f866466ef1a6b866803d4f23ba288ab888717
LauraJaime8 <laurajaimev8@gmail.com>
2018-04-03 20:17:38 +0200

    Add style to notes

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,16 +4,16 @@ class SessionController < ApplicationController
 
   def create
     @user = User.find_by name: params[:username]
-    if !@user
-      flash.now.alert = "username #{params[:username]} was invalid"
+      if !@user
+        flash.now.alert = "Username #{params[:username]} not sign up"
       render :new
-    elsif @user.password == params[:password]
-      session[:user] = @user.name
-      redirect_to root_url, :notice => "Logged in!"
-    else
-      flash.now.alert = "password was invalid"
-      render :new
-    end
+      elsif @user.password == params[:password]
+        session[:user] = @user.name
+        redirect_to root_url, :notice => "Logged in!"
+      else
+        flash.now.alert = "password was invalid"
+        render :new
+      end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+      redirect_to root_url, :notice => "#{@user.name} Signed up!"
     else
       render :new
     end
