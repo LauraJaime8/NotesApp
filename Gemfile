e08d677a929d38556b560ff3ad4e33df33cec95f
OLIVA GALVEZ GARCIA <Oliva.Galvez@alu.uclm.es>
2018-04-23 15:05:45 +0200

    Email campo

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
-
+gem 'wdm', '~> 0.1.0'
 gem 'rails-assets-sweetalert2', source: 'https://rails-assets.org'
 gem 'carrierwave'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412112453) do
+ActiveRecord::Schema.define(version: 20180423124901) do
 
   create_table "avatars", force: :cascade do |t|
     t.string "cover_filename"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180412112453) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "cover_filename"
+    t.string "email"
   end
 
 end
