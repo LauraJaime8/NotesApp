c1fc82432dc6ff31109421d579661287b8c605ee
LauraJaime8 <laurajaimev8@gmail.com>
2018-04-23 20:28:33 +0200

    Migracion para poder invitar a usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423124901) do
+ActiveRecord::Schema.define(version: 20180423181043) do
 
   create_table "avatars", force: :cascade do |t|
     t.string "cover_filename"
@@ -32,6 +32,30 @@ ActiveRecord::Schema.define(version: 20180423124901) do
     t.datetime "updated_at", null: false
     t.string "cover_filename"
     t.string "email"
+    t.string "email_invitation", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer "invitation_limit"
+    t.string "invited_by_type"
+    t.integer "invited_by_id"
+    t.integer "invitations_count", default: 0
+    t.index ["email_invitation"], name: "index_users_on_email_invitation", unique: true
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
+    t.index ["invitations_count"], name: "index_users_on_invitations_count"
+    t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by_type_and_invited_by_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
