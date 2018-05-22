a7ba432bd01128e1a7de3dcb3c26cf86ed88bd97
OLIVA GALVEZ GARCIA <Oliva.Galvez@alu.uclm.es>
2018-05-22 18:19:29 +0200

    Add Friend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :friendships
   get 'session/new'
 
   get 'session/create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180522090415) do
+ActiveRecord::Schema.define(version: 20180522161621) do
 
   create_table "collections", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.string "create"
+    t.string "destroy"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
