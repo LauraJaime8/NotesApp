81e387086881b992b5bc794c74493a333866c86f
OLIVA GALVEZ GARCIA <Oliva.Galvez@alu.uclm.es>
2018-05-25 18:43:54 +0200

    Hola

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180524100406) do
-
-  create_table "collections", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180525102502) do
 
   create_table "friend_requests", force: :cascade do |t|
     t.integer "user_id"
